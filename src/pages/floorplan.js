5b43f0e551ee2a6edac9c4328b24efbe607a1040
Ayusha Khadka <akhadkacs@gmail.com>
2022-11-23 14:02:16 -0600

    added thermostat

--- src/pages/floorplan.js
+++ src/pages/floorplan.js
@@ -1,22 +1,29 @@
-import { Container } from '@mui/material';
-import React from 'react';
-import NewNavbar from '../components/NewNavbar';
-import floorplan from '../models/floorplan2.png'
-import { Helmet } from 'react-helmet';
+import { Container } from "@mui/material";
+import React from "react";
+import NewNavbar from "../components/NewNavbar";
+import Thermostat from "../components/Thermostat";
+import floorplan from "../models/floorplan2.png";
+import { Helmet } from "react-helmet";
 const FloorPlan = () => {
   return (
     <div>
       <Helmet>
-        <style>{'body { background-color: 	#212529; }'}</style>
+        <style>{"body { background-color: 	#212529; }"}</style>
       </Helmet>
       <NewNavbar />
+      <Thermostat />
       <h1 align="center">secret text</h1>
-
       <div className="container">
-        <img align="center" src={floorplan} width="1300" height="600" alt="fireSpot"/>
+        <img
+          align="center"
+          src={floorplan}
+          width="1300"
+          height="600"
+          alt="fireSpot"
+        />
       </div>
     </div>
   );
 };
-  
+
 export default FloorPlan;
